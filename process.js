7eeca4f4d7b7dc453813ec9bb9d962baf150d5ce
Ebrahim Byagowi <ebrahim@gnu.org>
2017-05-15 11:11:24 +0430

    Update process.js

--- process.js
+++ process.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var child_process = require('child_process');
+var { spawnSync } = require('child_process');
 
 var i = 0;
 function kashida(text) {
@@ -7,10 +7,10 @@ function kashida(text) {
   if (i % 1000 === 0) {
     console.log('Reached to ' + i);
   }
-  return child_process.spawnSync(
+  return spawnSync(
     'kashida.exe',
     ['C:\\Windows\\Fonts\\tahoma.ttf', '20000'].concat(text.split('').map(x => x.charCodeAt(0)))
-  ).output.toString().replace(/,/g, '').trim().split('\n').map(x => x.split("'")[1]).join(',');
+  ).stdout.toString().trim().split('\n').map(x => x.split("'")[1]).join(',');
 }
 
 var all = Array.from(new Set(fs.readFileSync('train.conll').toString().split('\n').map(x => x.split('\t')[1])))
